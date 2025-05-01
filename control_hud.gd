ccacea705438f23f84f347db7f3608d960e6e9b5
DauntingOcean44 <116773144+DauntingOcean44@users.noreply.github.com>
2025-04-30 21:46:24 -0400

    Created weighted picker function
    Veto system is working at a basic level, but still requires visualization and comparing with the player's decision.

--- control_hud.gd
+++ control_hud.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 
+var wagerA
+var wagerB
+var wagerC 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +14,32 @@ func _process(_delta: float) -> void:
 	pass
 
 
+func _chose_wager():
+	$Control/WagerContainer/WagerA.disabled = true
+	$Control/WagerContainer/WagerB.disabled = true
+	$Control/WagerContainer/WagerC.disabled = true
+	
+func _assign_wagers(A, B, C):
+	$Control/WagerContainer/WagerA.text = "$" + str(A)
+	$Control/WagerContainer/WagerB.text = "$" + str(B)
+	$Control/WagerContainer/WagerC.text = "$" + str(C)
+	wagerA = A
+	wagerB = B
+	wagerC = C
+	$Control/WagerContainer/WagerA.disabled = false
+	$Control/WagerContainer/WagerB.disabled = false
+	$Control/WagerContainer/WagerC.disabled = false
+
 func _on_wager_a_button_up() -> void:
-	get_tree().call_group("wager_amount", "_cast_wager", 1)
+	get_tree().call_group("wager_amount", "_cast_wager", wagerA)
+	_chose_wager()
+
+
+func _on_wager_b_button_up() -> void:
+	get_tree().call_group("wager_amount", "_cast_wager", wagerB)
+	_chose_wager()
+	
+
+func _on_wager_c_button_up() -> void:
+	get_tree().call_group("wager_amount", "_cast_wager", wagerC)
+	_chose_wager()
--- main.gd
+++ main.gd
@@ -9,8 +9,12 @@ var quotaMax
 var turnCurr
 var turnMax
 
-var vetoArray
-var wagerArray
+#Indexes 0, 2, and 4 represent the wager, while 1, 3, 5 represent the amount
+var vetoArray = [0, 0, 0, 0, 0, 0]
+
+var wagerA
+var wagerB
+var wagerC
 
 var bankCurr
 
@@ -19,10 +23,65 @@ var gameState
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	matchCurr = 1
 	_menu_screen()
 	
+	matchCurr = 1
+	_next_match(gameDict["Match" + str(matchCurr)])
 	
 	
+#Level information dictionary
+var gameDict = {
+	"Match1": {
+		"Quota": 30,
+		"Turns": 20,
+		
+		"WagerA": 1,
+		"WagerB": 3,
+		"WagerC": 5,
+		
+		#First value is the wager, second value is the number of that wager
+		"VetoA": [1, 0],
+		"VetoB": [3, 7],
+		"VetoC": [5, 13]
+	}
+}
+
+func _random_weighted(array):
+	var totalWeight = array[1] + array[3] + array[5]
+	var randomIndex = ceil(randf_range(1, totalWeight))
+	var currIndex = 0
+	var vetoData
+		
+	print ("weight: " + str(totalWeight))
+	if totalWeight <= 0:
+		$VetoDelay.stop()
+		return "No more vetoes"
+		
+	currIndex += vetoArray[1]
+	if currIndex >= randomIndex:
+		vetoData = vetoArray[0]
+		array[1] -= 1
+		print ("Veto A Chosen")
+		return vetoData
+	
+	
+	currIndex += vetoArray[3]
+	if currIndex >= randomIndex:
+		vetoData = vetoArray[2]
+		array[3] -= 1
+		print ("Veto B Chosen")
+		return vetoData
+		
+	currIndex += vetoArray[5]
+	if currIndex >= randomIndex:
+		vetoData = vetoArray[4]
+		array[5] -= 1
+		print ("Veto C Chosen")
+		return vetoData
+		
+			
+
 #When the game is reset / opened
 func _menu_screen():
 	$MenuHUD.show()
@@ -39,9 +98,36 @@ func _instance_game():
 	$ControlHUD.show()
 	print("WORKS!")
 	
+	#Remove after
+	$VetoDelay.start()
+	
+#When the next match occurs
+func _next_match(dictCurr):
+	quotaCurr = 0
+	turnCurr = 0
+	vetoArray[0] = dictCurr["VetoA"][0]
+	vetoArray[1] = dictCurr["VetoA"][1]
+	vetoArray[2] = dictCurr["VetoB"][0]
+	vetoArray[3] = dictCurr["VetoB"][1]
+	vetoArray[4] = dictCurr["VetoC"][0]
+	vetoArray[5] = dictCurr["VetoC"][1]
+	
+	wagerA = dictCurr["WagerA"]
+	wagerB = dictCurr["WagerB"]
+	wagerC = dictCurr["WagerC"]
+	quotaMax = dictCurr["Quota"]
+	turnMax = dictCurr["Turns"]
+	
+	get_node("ControlHUD")._assign_wagers(wagerA, wagerB, wagerC)
+
 func _cast_wager(amount):
 	print (amount)
+	get_node("ControlHUD/Control/CommitTable/WagerChoice").text = "Wager: $" + str(amount)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_veto_delay_timeout():
+	print(_random_weighted(vetoArray))
