55101449b1a3fadf51a1b918da6435b76225f382
DauntingOcean44 <116773144+DauntingOcean44@users.noreply.github.com>
2025-05-20 21:06:56 -0400

    Toggle Between % and x
    You can now toggle between the percentage probability of a slot and the amount of times it appeared.

--- roulette_hud.gd
+++ roulette_hud.gd
@@ -6,6 +6,8 @@ var SliderNode
 var PValue
 var EValue
 var AmountA
+var ToggleButton
+var displayPercent = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +16,14 @@ func _ready():
 	PValue = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer2/HBoxContainer/VBoxContainer/HBoxContainer/PValue
 	EValue = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer2/HBoxContainer/VBoxContainer/HBoxContainer2/EValue
 	AmountA = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer2/HBoxContainer/PanelContainer/HBoxContainer/AmountA
+	ToggleButton = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer2/HBoxContainer/VBoxContainer2/ToggleButton
 	_run_1000_trials()
+	_update_other_values()
+	_display_values()
+	
+	_run_1000_trials()
+	_update_other_values()
+	_display_values()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,26 +31,54 @@ func _process(delta: float) -> void:
 	pass
 
 
-
-func _on_start_button_button_up():
-	_run_1000_trials()
-
 func _run_1000_trials():
 	#Initialize the array
 	displayTable.clear()
 	
-	displayTable.append(["00", 0])
+	displayTable.append(["00", 0, 0.0])
 	for i in range(37):
-		displayTable.append([str(i), 0])
+		displayTable.append([str(i), 0, 0.0])
 		
 	#Run set amount of trials
 	for i in range(trialSize):
 		var index = floor(randf_range(0, 38))
 		displayTable[index][1] += 1
 	
+	#Calculate the probability for each trial
 	for i in range($PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer/GridContainer.get_child_count()):
 		var child = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer/GridContainer.get_child(i)
-		child.text = str(displayTable[i][0]) + ": " + str(displayTable[i][1]) + "x"
+		var probability = displayTable[i][1] / trialSize
+		probability *= 100
+		probability = snapped(float(probability), float(0.01))
+		displayTable[i][2] = probability
+	
+	_update_other_values()
+	_display_values()
+	
+func _toggle_display():
+	if !displayPercent:
+		displayPercent = true
+	else:
+		displayPercent = false
+		
+	if displayPercent:
+		ToggleButton.text = "Toggle x"
+	else:
+		ToggleButton.text = "Toggle %"
+		
+	_display_values()
+		
+	
+		
+		
+
+func _display_values():
+	for i in range($PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer/GridContainer.get_child_count()):
+		var child = $PanelContainer/VBoxContainer/ControlBody/CenterContainer/VBoxContainer/PanelContainer/GridContainer.get_child(i)
+		if !displayPercent:
+			child.text = str(displayTable[i][0]) + ": " + str(displayTable[i][1]) + "x"
+		else:
+			child.text = str(displayTable[i][0]) + ": " + str(displayTable[i][2]) + "%"
 	
 	
 func _update_other_values():
@@ -51,7 +88,6 @@ func _update_other_values():
 	floatA = float(1) / float(38)
 	floatA = snapped(floatA, 0.0001)
 	floatA *= 100
-	print(floatA)
 	PValue.text = str(floatA) + "%"
 
 	
@@ -67,3 +103,10 @@ func _on_trial_slider_drag_ended(value_changed: bool):
 	
 	
 	
+func _on_toggle_button_button_up():
+	_toggle_display()
+
+
+func _on_start_button_button_up():
+	_run_1000_trials()
+	_update_other_values()
